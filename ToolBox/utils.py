865dfd0b00540ee9b3e6eaa38aa67e1ad2990491
Mo Yang <mo_yang@foxmail.com>
2021-01-31 11:23:35 +0100

    add function to downcast numerical columns

--- ToolBox/utils.py
+++ ToolBox/utils.py
@@ -79,3 +79,25 @@ def simple_time_tracker(method):
         return result
 
     return timed
+
+def df_optimized(df, verbose=True, **kwargs):
+    """
+    Reduces size of dataframe by downcasting numerical columns
+    :param df: input dataframe
+    :param verbose: print size reduction if set to True
+    :param kwargs:
+    :return:
+    """
+    in_size = df.memory_usage(index=True).sum()
+    for type in ["float", "integer"]:
+        l_cols = list(df.select_dtypes(include=type))
+        for col in l_cols:
+            df[col] = pd.to_numeric(df[col], downcast=type)
+            if type == "float":
+                df[col] = pd.to_numeric(df[col], downcast="integer")
+    out_size = df.memory_usage(index=True).sum()
+    ratio = (1 - round(out_size / in_size, 2)) * 100
+    GB = out_size / 1000000000
+    if verbose:
+        print("optimized size by {} % | {} GB".format(ratio, GB))
+    return df
